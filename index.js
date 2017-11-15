4de7bb48b4c23c98f8d6d1342c2a05426a4321b0
Hacktiv8 <hacktiv8-6@Hacktiv8-6.local>
2017-11-15 11:24:09 +0700

    Release 1: Add getObj Function

--- index.js
+++ index.js
@@ -8,8 +8,21 @@ function arrOfObj(name, phase, gender) {
   arr.push(obj);
   obj = {};
 }
-
+function getObj(name) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i]['name'] == name) {
+      return arr[i];
+    }
+  }
+  return -1;
+}
 arrOfObj('Akbar', 1, 'Male');
 arrOfObj('Icha', 1, 'Female');
+arrOfObj('Adith', 2, 'Male');
+arrOfObj('Tama', 3, 'Male');
+arrOfObj('Rizky', 0, 'Male');
 
 console.log(arr);
+
+console.log(getObj('Icha'));
+console.log(getObj('Bambang'));
