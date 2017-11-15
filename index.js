34df97374753504fca9ab6a0b94382ff2cd743fc
Haniiful Wahib <wahib@Haniifuls-MacBook-Pro.local>
2017-11-15 12:08:10 +0700

    initial submit for Release 0, additional "NOT FOUND" feature

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var arr = [];
 
+// Release 0
 function createObj(name, phase, gender){
   // YOUR CODE HERE
   let obj = {};
@@ -12,12 +13,29 @@ function createObj(name, phase, gender){
   return arr.push(obj);
 }
 
+//Release 1
+function getData(name){
+  // YOUR CODE HERE . .
+  for(let i = 0; i < arr.length ; i++){
+    if(arr[i].name === name){
+      return arr[i]
+    }
+  }
+  return 'NOT FOUND'
 
-// DRIVER
+}
+
+//  ===== DRIVER =====
+// Release 0
 createObj('Akbar', 1, 'male');
 createObj('Icha', 1, 'female');
 createObj('Adhit', 2, 'male');
 createObj('Tama', 2, 'male');
 createObj('Rifky', 3, 'male');
-
 console.log(arr);
+
+// Release 1
+console.log(getData('Icha'));
+console.log(getData('Adhit'));
+console.log(getData('Akbar'));
+console.log(getData('Wahib'));
