bee87cc5dca06795e362b242352dd8a6d0ab9a0a
aldiws <syldie.aldiwijaya@gmail.com>
2017-11-15 18:41:18 +0700

    array of object done

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ function createObj(name, phase, gender){
 createObj('Akbar',1,'male')
 createObj('Icha',1,'female')
 
-console.log(arr)
+console.log(arr[0]['name'])
+
+
+function getData (name){    
+    for(var i=0;i<arr.length;i++){
+      if(arr[i]['name'] === name){
+        return arr[i]
+      }  
+    }  
+    return false
+  }
+  console.log(getData('Icha'))
