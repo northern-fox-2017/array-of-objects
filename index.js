e8760ae2ad35bd89f44fb3cd1421262eda9cdc45
Fajar Patappari <top.sick.red@gmail.com>
2017-11-15 15:35:14 +0700

    Array of Objects Release 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 let arr = []
+
 var createObject = (name, phase, gender) => {
   arr.push({
     name,
@@ -8,8 +9,16 @@ var createObject = (name, phase, gender) => {
   })
 }
 
+var getData = name => {
+  for (let item of arr) {
+    if (item.name === name) {
+      return item
+    }
+  }
+}
+
 createObject('fajar', 120, 'male')
 createObject('arief', 14, 'female')
 createObject('aldy', 14, 'male')
 
-console.log(arr)
+console.log(getData('fajar'))
